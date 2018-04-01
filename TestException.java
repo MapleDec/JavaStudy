ae1338ab4b1d152a2c25722b53776850fe678b4e
MapleDec <30573358+MapleDec@users.noreply.github.com>
2018-04-01 20:06:17 +0800

    Test of exception

---
+++ TestException.java
@@ -0,0 +1,21 @@
+public class TestException{
+	public static void main(String[] args){
+		int[] a = {1,2,3,4,5};
+		try{
+			System.out.println(a[5]);
+		}
+		catch(Exception e){
+			System.out.println("The system is fixing, please wait a minute.");
+			e.printStackTrace();
+		}
+		
+		void m(int i){
+			int b=10;
+			int c = 0;
+			
+		}
+	
+	
+	}
+}
+
