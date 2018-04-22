1e124fd59777aec6227e81b227ccf69094d5aae7
MapleDec <30573358+MapleDec@users.noreply.github.com>
2018-04-22 22:23:11 +0800

    Add count word frequency
    1. Map interfae
    2. Integer
    3. Auto packagr and resolve

--- RongQi/TestMap.java
+++ RongQi/TestMap.java
@@ -1,6 +1,10 @@
 import java.util.*;
 
 public class TestMap{
+	
+	//public static final Integer ONE = new Integer(1);
+	// ==
+	public static final int ONE = 1;
 	public static void main(String[] args){
 		Map m1 = new HashMap();
 		Map m2 = new TreeMap();
@@ -12,7 +16,7 @@ public class TestMap{
 		m1.put("One", 1);
 		m1.put("Two", 2);
 		m1.put("Three", 3);
-		
+
 		m2.put("A",new Integer(1));
 		m2.put("B",new Integer(2));
 		
@@ -30,5 +34,21 @@ public class TestMap{
 		Map m3 = new HashMap(m1);
 		m3.putAll(m2);
 		System.out.println(m3);
+		
+		// Add test args: count word frequency
+		Map m = new HashMap();
+		for(int i = 0 ;i<args.length;i++){
+			//Integer fre = (Integer)m.get(args[i]);
+			// == Auto package and resolve
+			int fre = (Integer)m.get(args[i])==null? 0:(Integer)m.get(args[i]);
+			
+			// give object to m according fre value
+			//m.put(args[i],(fre==null? ONE: 	new Integer(fre.intValue()+1)));
+			// ==
+			m.put(args[i],(fre==0 ? ONE:fre+1));
+		}
+		System.out.println("distinct word detected: "+ m.size());
+		System.out.println(m);
+
 	}
 }
